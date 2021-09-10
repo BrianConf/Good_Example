b6217439dc4e7bb468be9b6d0de8a88999972a25
BrianConf <brain.confidence@gmail.com>
2021-09-10 12:12:35 +0200

    Exercise 2.1

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -6,16 +6,3 @@
 
 using namespace std; // cout, endl, complex are all part of this namespace
 
-int main()
-{
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use 
-
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
-
-	//ret erye w
-	// answer++;
-
-	return 0;
-}
-
